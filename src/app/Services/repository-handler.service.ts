2d2e6a984be2b7c44970dda77d268e3f39315b52
Andreas Boll Nielsen <andreas@boll-nielsen.dk>
2024-04-15 16:00:47 +0200

    implemented getweather method i repo

--- src/app/Services/repository-handler.service.ts
+++ src/app/Services/repository-handler.service.ts
@@ -1,9 +1,22 @@
+import { InputModel } from './../Model/input-model';
 import { Injectable } from '@angular/core';
+import { ApiHandlerService } from './api-handler.service';
+import { DailyWeatherModel } from '../Model/daily-weather-model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RepositoryHandlerService {
+  weatherDate: DailyWeatherModel[];
 
-  constructor() { }
+  constructor(private api:ApiHandlerService) { }
+
+  public async getWeatherData(inputData: InputModel): Promise<void> {
+    try {
+      const response: any = await this.api.GetWeatherData(inputData);
+      return response;
+    } catch (error) {
+      console.error('Error fetching weather data:', error);
+    }
+  }
 }
